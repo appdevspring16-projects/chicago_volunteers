c4e23b04f3f61a9e43ff45e17a49bb2ca76a2a42
Raghu Betina <raghu@firstdraft.com>
2016-12-05 02:06:43 +0000

    Integrate Ransack

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -10,7 +10,8 @@ class ProjectsController < ApplicationController
   end
 
   def index
-    @projects = Project.page(params[:page]).per(10)
+    @q = Project.ransack(params[:q])
+    @projects = @q.result(:distinct => true).includes(:organization, :location, :day).page(params[:page]).per(10)
 
     render("projects/index.html.erb")
   end
